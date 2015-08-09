3e45f0ff86cd587600fc0f04b174576eb17f22c6
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2015-08-09 17:36:35 +0700

    Add test for new deck from card form

---
+++ spec/features/card_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe "Card form" do
+  before(:each) do
+    login(user, "abracadabra")
+  end
+
+  let!(:user) { create(:user, password: "abracadabra") }
+
+  it "creates deck from card form" do
+    visit new_card_path
+    fill_in "Исходный текст", with: "Abracadabra"
+    fill_in "Перевод текста", with: "Абракадабра"
+    fill_in "Создать новую колоду:", with: "Новая колода"
+    click_button "Создать карточку"
+    expect(page).to have_content "Новая колода"
+  end
+end
