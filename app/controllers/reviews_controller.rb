b278018fd61dead7287a69fbcbf34da9d30d77fb
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2015-08-17 09:54:02 +0700

    Modify flash messages for reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -7,16 +7,16 @@ class ReviewsController < ApplicationController
     @card = Card.find(review_params[:card_id])
     results = @card.check_answer(review_params[:answer])
     if results[:success]
-      flash[:notice] = "#{t(:right_answer)} #{@card.original_text}."
+      flash[:notice] = t(:right_answer, card_text: @card.original_text)
       if results[:typos] > 0
-        flash[:notice] += " #{t(:typo_message)} #{review_params[:answer]}"
+        flash[:notice] += t(:typo_message, answer: review_params[:answer])
       end
       redirect_to new_review_path
     elsif @card.attempt > 0
       flash.now[:error] = t(:next_try)
       render :new
     else
-      flash[:error] = "#{t(:mistake_message)} #{@card.original_text}"
+      flash[:error] = t(:mistake_message, card_text: @card.original_text)
       redirect_to new_review_path
     end
   end
