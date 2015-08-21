9bdc32d6802b7e4819841653ca6717900899488f
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2015-08-21 20:15:45 +0700

    Fix Hound reviews

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
     port: 587,
     user_name: ENV["MANDRILL_USERNAME"],
     password: ENV["MANDRILL_APIKEY"],
-    authentication: :plain    
+    authentication: :plain
   }
 
   # Print deprecation notices to the Rails logger.
