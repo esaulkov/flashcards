2547b1fc71cad2f891790a886c50d55cb963b376
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2015-08-26 11:44:18 +0700

    Display new review without reload

--- app/assets/javascripts/review.js.coffee
+++ app/assets/javascripts/review.js.coffee
@@ -1,10 +1,16 @@
-$(document).ready ->
-  startTime = $.now()
-
-  $('#review_form').submit (e) ->
+calculateTime = (startTime) -> $('#review_form').submit (e) ->
     finalTime = $.now()
     answerTime = (finalTime - startTime) / 1000
     $('<input />').attr('type', 'hidden')
       .attr('name', 'review[answer_time]')
       .attr('value', answerTime)
       .appendTo('#review_form')
+
+$(document).ready ->
+  startTime = $.now()
+  calculateTime(startTime)
+
+$(document).ajaxComplete ->
+  startTime = $.now()
+  calculateTime(startTime)
+
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,23 +5,19 @@ class ReviewsController < ApplicationController
 
   def create
     @card = Card.find(review_params[:card_id])
-    results = @card.check_answer(review_params[:answer],
-                                 review_params[:answer_time])
+    @results = @card.check_answer(review_params[:answer],
+                                  review_params[:answer_time])
     respond_to do |format|
       format.js do
-        if results[:success]
-          flash[:notice] = t(:right_answer, card_text: @card.original_text)
-          if results[:typos] > 0
-            flash[:notice] += t(:typo_message, answer: review_params[:answer])
+        if @results[:success]
+          flash.now[:notice] = t(:right_answer, card_text: @card.original_text)
+          if @results[:typos] > 0
+            flash.now[:notice] += t(:typo_message, answer: review_params[:answer])
           end
-          redirect_to new_review_path
-          # @card = current_user.card_for_review
         elsif @card.attempt > 0
           flash.now[:error] = t(:next_try)
         else
-          flash[:error] = t(:mistake_message, card_text: @card.original_text)
-          # @card = current_user.card_for_review
-          redirect_to new_review_path
+          flash.now[:error] = t(:mistake_message, card_text: @card.original_text)
         end
       end
     end
