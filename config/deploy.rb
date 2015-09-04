83e36465c85694dc38629cbc17f3d6c8f7aab994
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2015-09-04 20:18:32 +0700

    Remove rvm from Capistrano config

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,9 +15,6 @@ set :linked_files, fetch(:linked_files, []).push("config/database.yml", ".env")
 set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system")
 set :keep_releases, 5
 
-set :rvm_type, :user
-set :rvm_ruby_version, "ruby-2.2.0"
-
 set :puma_state, "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 # accept array for multi-bind
