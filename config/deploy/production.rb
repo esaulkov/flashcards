607671ff6e6720271f007a594dc2ccf8b02b2b8b
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2015-09-04 21:48:39 +0700

    Add AWS credentials to Capistrano task

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,8 +2,10 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-AWS.config(access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-           secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
-           region: "us-west-2")
+require "aws-sdk"
+credentials = YAML.load_file("config/aws.yml")
+puts credentials
+AWS.config(credentials)
+
 ec2_instance = AWS.ec2.instances.with_tag("Name", "cards-app").first
 server ec2_instance.ip_address, user: "deploy", roles: %w{web app db}
