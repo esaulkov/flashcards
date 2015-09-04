36c0e6fc5a22a730e09f8cdeedd36635105e5ab4
Evgeny Esaulkov <evg.esaulkov@gmail.com>
2015-09-04 21:00:54 +0700

    Get server address by EC2 API

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,8 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-server "52.24.120.135", user: "deploy", roles: %w{web app db}
+AWS.config(access_key_id: ENV["AWS_ACCESS_KEY_ID"],
+           secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+           region: 'us-west-2')
+ec2_instance = AWS.ec2.instances.with_tag('Name', 'cards-app').first
+server ec2_instance.ip_address, user: "deploy", roles: %w{web app db}
